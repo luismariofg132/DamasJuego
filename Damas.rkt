7df3da78f94fe8a4c4857bd89e61079f2e21f9f7
Luis Mario Franco <lmfg06@gmail.com>
2022-10-25 21:48:08 -0500

    Dibujar tablero

--- Damas.rkt
+++ Damas.rkt
@@ -5,4 +5,35 @@
 (open-graphics)
 
 ; Dimensiones de la ventana del tablero
-(define dimensiones)
+(define dimensionX 700)
+(define dimensionY 700)
+
+; Cantidad de cuadros
+(define cantidadCuadros 64)
+
+; Dimensiones cuadros
+(define dimensionCuadros 75)
+
+; Sumatoria de pixeles
+(define sumaPx 75)
+
+; Creacion de la ventana
+(define ventana(open-viewport "Damas - Programacion 1" dimensionX dimensionY))
+;((draw-rectangle ventana (make-posn 50 50) dimensionCuadros dimensionCuadros "black"))
+
+
+; Funcion recursiva encargada de pintar el tablero
+(define (dibujarTablero cantidadNeto cantidadTotal color posX posY cantidadFila)
+  (if (equal? color 1)
+      ((draw-solid-rectangle ventana)(make-posn posX posY) dimensionCuadros dimensionCuadros "black")
+      ((draw-rectangle ventana)(make-posn posX posY) dimensionCuadros dimensionCuadros "black"))
+  (cond
+    [(equal? cantidadNeto cantidadTotal)(display "Tablero dibujado")]
+    [(and (equal? cantidadFila 7) (equal? color 1))(dibujarTablero (+ cantidadNeto 1) cantidadTotal 1 50 (+ posY sumaPx) 0)]
+    [(and (equal? cantidadFila 7) (equal? color 2))(dibujarTablero (+ cantidadNeto 1) cantidadTotal 2 50 (+ posY sumaPx) 0)]
+    [(equal? color 1)(dibujarTablero (+ cantidadNeto 1) cantidadTotal 2 (+ posX sumaPx) posY (+ cantidadFila 1))]
+    [(equal? color 2)(dibujarTablero (+ cantidadNeto 1) cantidadTotal 1 (+ posX sumaPx) posY (+ cantidadFila 1))]
+    )
+  )
+
+(dibujarTablero 1 cantidadCuadros 1 50 50 0)
