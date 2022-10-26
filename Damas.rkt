fa6bca744f77a2e5cc8e122071ece1ab6836db24
Luis Mario Franco <lmfg06@gmail.com>
2022-10-25 22:29:17 -0500

    Dibujar guias

--- Damas.rkt
+++ Damas.rkt
@@ -1,8 +1,13 @@
 #lang racket
 (#%require (only racket/base random))
 (require (lib "Graphics.ss" "graphics"))
-
 (open-graphics)
+#|
+---------------------------------------------------------------------------------
+                      Inicio variables de configuracion del tablero
+                                     no mover!!!!
+---------------------------------------------------------------------------------
+|#
 
 ; Dimensiones de la ventana del tablero
 (define dimensionX 700)
@@ -17,9 +22,35 @@
 ; Sumatoria de pixeles
 (define sumaPx 75)
 
+; Define el titulo de la ventana
+(define tituloVentana "Juego Damas - Programacion 1 - Juan D Malagon - Susana M Perez - Luis M Franco")
+
+; Define dos listas para las guias del tablero
+(define listaLetras (list "A" "B" "C" "D" "E" "F" "G" "H"))
+(define listaNumeros (list "1" "2" "3" "4" "5" "6" "7" "8"))
+
+; Define el incremento en (X,Y) de las guias
+(define incXYGuia 75)
+
+
+#|
+---------------------------------------------------------------------------------
+                    Fin de las variables de configuracion del tablero                                    
+---------------------------------------------------------------------------------
+|#
+
 ; Creacion de la ventana
-(define ventana(open-viewport "Damas - Programacion 1" dimensionX dimensionY))
-;((draw-rectangle ventana (make-posn 50 50) dimensionCuadros dimensionCuadros "black"))
+(define ventana(open-viewport tituloVentana dimensionX dimensionY))
+
+; Funcion recursiva encargada de dibujar las guias del tablero
+(define (dibujarGuias cantidadNeto cantidadTotal posX posY)
+  ((draw-string ventana) (make-posn posX 40) (list-ref listaLetras cantidadNeto) "black")
+  ((draw-string ventana) (make-posn 25  posY) (list-ref listaNumeros cantidadNeto) "black")
+  (cond
+    [(equal? cantidadNeto cantidadTotal)(display "Guias dibujadas")]
+    [else (dibujarGuias (+ cantidadNeto 1) cantidadTotal  (+ posX incXYGuia) (+ posY incXYGuia))]
+    )
+  )
 
 
 ; Funcion recursiva encargada de pintar el tablero
@@ -36,4 +67,15 @@
     )
   )
 
-(dibujarTablero 1 cantidadCuadros 1 50 50 0)
+#|
+------------------------------------------------------------------------------
+                              Disparador de eventos
+------------------------------------------------------------------------------
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+|#
+
+(define (disparador)
+  (dibujarTablero 1 cantidadCuadros 1 50 50 0)
+  (dibujarGuias 0 (- (length listaLetras) 1) 75 100 )
+  )
+(disparador)
